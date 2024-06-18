0e029b418189ff7ff90c9f29853a2bd36dee7b6c
Oleg Work <zeus.set@gmail.com>
2024-06-18 15:46:59 +0300

    Added second program

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-"""Check whether the site is available"""
+"""Check whether the site is available
 import requests
 r = requests.get('https://google.com')
-print(r.status_code)
+print(r.status_code)"""
+
+#___________________________________
+
+"""amount = 20
+cookie_price = 10
+candy_price = 5
+
+rest = amount - cookie_price
+
+if rest >= candy_price:
+    print("You can have a candy!")
+else:
+    print("No money, no honey!")"""
+
+#___________________________________
