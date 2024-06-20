31766879c775b9d940c0c4d7f3b3f5c58a89465e
Oleg Work <zeus.set@gmail.com>
2024-06-20 22:57:32 +0300

    type, isinstance exemples

--- main.py
+++ main.py
@@ -18,3 +18,23 @@ else:
 
 #___________________________________
 
+random_number = 999
+print("Random number is: ",random_number)
+
+type_random_number = type(random_number)
+print("Random number type is: ", type_random_number)
+
+random_bool = True
+print("Random number is: ", random_bool)
+
+type_random_bool = type(random_bool)
+print("Random number type is: ", type_random_bool)
+
+random_string = "Order received. Expect results."
+print("Random number is: ", random_string)
+
+type_random_string = type(random_string)
+print("Random number type is: ", type_random_string)
+
+#check whether the variable is suggested type
+print("Random string is string: ", isinstance(random_string, str))
